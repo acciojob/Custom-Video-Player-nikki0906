71346593db919a18ffc098e791da52474f54932e
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-06-01 12:39:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/* Edit this file */
 // Get elements
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
@@ -12,3 +11,58 @@ const backwardButton = player.querySelector('.backward');
 const forwardButton = player.querySelector('.forward');
 
 // Functions
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  const icon = video.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function handleVolume() {
+  video.volume = volume.value;
+}
+
+function handlePlaybackSpeed() {
+  video.playbackRate = playbackSpeed.value;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function backward() {
+  video.currentTime -= 10;
+}
+
+function forward() {
+  video.currentTime += 25;
+}
+
+// Event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+toggle.addEventListener('click', togglePlay);
+progress.addEventListener('click', scrub);
+volume.addEventListener('input', handleVolume);
+playbackSpeed.addEventListener('input', handlePlaybackSpeed);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+backwardButton.addEventListener('click', backward);
+forwardButton.addEventListener('click', forward);
